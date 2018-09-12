6bbb07bad8184b7056871b34cacdb8b4ccc0217b
Den <denv928@gmail.com>
2018-09-12 20:58:56 +0300

    Add rollup

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -6,7 +6,6 @@ module.exports = [
   './gulp/tasks/pug.js',
   './gulp/tasks/watch.js',
   './gulp/tasks/js.process.js',
-  './gulp/tasks/js.lint.js',
   './gulp/tasks/copy.image.js',
   './gulp/tasks/sprite.svg.js'
 ];
--- gulp/tasks/js.process.js
+++ gulp/tasks/js.process.js
@@ -1,11 +1,30 @@
-'use strict';
+const cleanup = require("rollup-plugin-cleanup");
+const {plugin: analyze} = require("rollup-plugin-analyzer");
+const buble = require("rollup-plugin-buble");
+const {uglify} = require("rollup-plugin-uglify");
+const rollup = require('rollup');
 
-module.exports = function() {
-  $.gulp.task('js:process', function() {
-    return $.gulp.src($.path.app)
-      .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.concat('app.js'))
-      .pipe($.gp.sourcemaps.write())
-      .pipe($.gulp.dest($.config.root + '/assets/js'))
-  })
+module.exports = function () {
+    $.gulp.task('js.process', function () {
+        return rollup.rollup({
+            input: './source/js/main.js',
+            plugins: [
+                buble(),
+                uglify(),
+                cleanup({
+                    comments: ['eslint']
+                }),
+                analyze()
+            ]
+        }).then(bundle => {
+            return bundle.write({
+                file: './app/assets/js/bundle.js',
+                format: 'iife',
+                name: 'bundle',
+                sourcemap: true
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    })
 };
